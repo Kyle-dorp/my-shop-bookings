814e35c3ac5715844381d882597382afdadebc91
Kyle <pruittk032@gmail.com>
2026-06-27 21:17:46 -0600

    fix session cookies behind railway proxy

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const { initDB } = require('./db');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.set('trust proxy', 1);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
